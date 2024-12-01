8d62eb7b60e3f830376b5b63917377c67379a96a
Sergey <gsv475@gmail.com>
2024-12-01 19:31:11 +0300

    feat: homework lesson 1

---
+++ src/lesson1/homework/DataTypesAndOperators.java
@@ -0,0 +1,68 @@
+package lesson1.homework;
+
+public class DataTypesAndOperators {
+    public static void main(String[] args) {
+        printValueFormula();
+        sumDigitsTwoDigitNumber();
+        sumDigitsThreeDigitNumber();
+        roundNumber();
+        printDivisionResult();
+        swapVariableValues();
+    }
+    // Task 1
+    public static void printValueFormula() {
+        int b = 2;
+        int c = 3;
+        double a = 0;
+        a = 4.0 * (b + c - 1)/2;
+        System.out.println("Formula calculation result " + a);
+    }
+
+    // Task 2
+    public static void sumDigitsTwoDigitNumber() {
+        int n = 26;
+        int firstNumber = n % 10;
+        int twoNumber = (n - firstNumber) / 10;
+        int result = firstNumber + twoNumber;
+
+        System.out.println("Sum of the digits of a two-digit number " + result);
+    }
+
+    // Task 3
+    public static void sumDigitsThreeDigitNumber() {
+        int n = 126;
+        int firstNumber = n % 10;
+        int twoNumber = (n - firstNumber) / 10 % 10;
+        int threeNumber = ((n - firstNumber - twoNumber * 10) / 100 % 10);
+        int result = firstNumber + twoNumber + threeNumber;
+        System.out.println("Sum of the digits of a three-digit number " + result);
+    }
+
+    // Task 4
+    public static void roundNumber() {
+        double n = 12.6;
+        int result = (int) (n + 0.5);
+        System.out.println("The value of the rounded number " + result);
+    }
+
+    // Task 5
+    public static void printDivisionResult() {
+        int q = 21;
+        int w = 8;
+        int wholePart = q / w;
+        int remainder = q % w;
+        System.out.println(wholePart + " and " + remainder +" remainder");
+    }
+
+    // Task 6
+    public static void swapVariableValues() {
+        int a = 1;
+        int b = 2;
+        a = a + b;
+        b = a - b;
+        a = a - b;
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+    }
+
+}
