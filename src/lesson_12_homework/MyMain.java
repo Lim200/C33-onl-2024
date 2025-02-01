cbbf3235c960fa7339c299a630c23519b10e6529
Sergey <gsv475@gmail.com>
2025-02-01 18:54:58 +0300

    fix: fix homework 12

--- src/lesson_12_homework/MyMain.java
+++ src/lesson_12_homework/MyMain.java
@@ -7,7 +7,7 @@ import java.util.regex.Pattern;
 public class MyMain {
     public static void main(String[] args) {
         task1();
-        //task2();
+        task2();
     }
 
     public static void task1() {
