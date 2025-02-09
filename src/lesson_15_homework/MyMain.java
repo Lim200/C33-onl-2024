cb39767744e7991b29ffd53f8b0d5e4056cdc22d
Sergey <gsv475@gmail.com>
2025-02-09 19:31:18 +0300

    feat: add homework 15

--- src/lesson_15_homework/MyMain.java
+++ src/lesson_15_homework/MyMain.java
@@ -1,14 +1,18 @@
 package lesson_15_homework;
 
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Scanner;
-import java.util.Set;
+import java.util.*;
 
 public class MyMain {
     public static void main(String[] args) {
-        //Task1();
-        //Task2();
+        Task1();
+        Task2();
+        List<Student> students = new ArrayList<>();
+        students.add(new Student("Ivan","Group1", 1, Arrays.asList(0,1,2,3)));
+        students.add(new Student("Masha","Group2", 1, Arrays.asList(9,5,8,5)));
+        students.add(new Student("Dima","Group3", 1, Arrays.asList(8,8,9,5)));
+
+        Student.removeTransferStudents(students);
+        printStudents(students, 2);
     }
 
     public static void Task1() {
@@ -34,6 +38,15 @@ public class MyMain {
 
         setAnimals.removeAnimal();
         System.out.println("Animals in the collection after removal: " + setAnimals.getAnimalNames());
+    }
+
+    public static void printStudents(List<Student> students, int course) {
+        System.out.println("------------ Task3 ------------");
+        for (Student student:students) {
+            if (student.getCourse() == course) {
+                System.out.println(student.getName());
+            }
+        }
     }
 
 
--- src/lesson_15_homework/Student.java
+++ src/lesson_15_homework/Student.java
@@ -1,4 +1,65 @@
 package lesson_15_homework;
 
+import java.util.Iterator;
+import java.util.List;
+
 public class Student {
+
+
+    private String name;
+    private String group;
+    private int course;
+    private List<Integer> subjectGrades;
+
+
+
+    public Student(String name, String group, int course, List<Integer> subjectGrades) {
+        this.name = name;
+        this.group = group;
+        this.course = course;
+        this.subjectGrades = subjectGrades;
+    }
+
+    public int getCourse() {
+        return course;
+    }
+
+    public String getGroup() {
+        return group;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public List<Integer> getSubjectGrades() {
+        return subjectGrades;
+    }
+
+    public void setCourse(int course) {
+        this.course = course;
+    }
+
+    public double getAverageGrade() {
+        if (subjectGrades.isEmpty()) return 0.0;
+        int sum = 0;
+        for(int grade:subjectGrades) {
+            sum +=grade;
+        }
+        return (double) sum / subjectGrades.size();
+    }
+
+    public static void removeTransferStudents(List<Student> students) {
+        Iterator<Student> iterator = students.iterator();
+        while (iterator.hasNext()) {
+            Student student = iterator.next();
+            if (student.getAverageGrade() < 3) {
+                iterator.remove();
+            } else {
+                student.setCourse(student.getCourse() + 1);
+            }
+        }
+    }
 }
+
+
